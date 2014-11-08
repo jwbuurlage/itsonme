faf41959cfedb8fd4b346f764cf1392646f7f726
Tom Bannink <tombannink@gmail.com>
2014-11-08 17:13:58 +0100

    ServerInterface

---
+++ android/app/src/main/java/hack/goodnight/itsonme/ServerInterface.java
@@ -0,0 +1,17 @@
+package hack.goodnight.itsonme;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+
+/**
+ *
+ * Created by tom on 11/8/14.
+ */
+public interface ServerInterface {
+    @POST("/groups")
+    void getGroup(Callback<Group> callback);
+}
