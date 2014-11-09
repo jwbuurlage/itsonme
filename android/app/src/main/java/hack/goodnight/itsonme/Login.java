55e4de427514c79d69ac3fc936fa591440ad01da
jwbuurlage <jwbuurlage@me.com>
2014-11-09 08:28:27 +0000

    light sans-serif, minor changes to UI

--- android/app/src/main/java/hack/goodnight/itsonme/Login.java
+++ android/app/src/main/java/hack/goodnight/itsonme/Login.java
@@ -88,11 +88,6 @@ public class Login extends FragmentActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
